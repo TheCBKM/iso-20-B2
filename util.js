affe12e054989e953e86c89fb0f278848d23d4e1
cbkm <rjoshi.prober@gmail.com>
2020-02-23 20:46:47 +0530

    UI/UX MVP

--- util.js
+++ util.js
@@ -1,9 +1,18 @@
-// const url = 'https://isogst.herokuapp.com/'
-const url = 'http://127.0.0.1:3000/'
+const url = 'https://isogst.herokuapp.com/'
+// const url = 'http://127.0.0.1:3000/'
 
+function logout(){
+    localStorage.clear()
+    window.location.href="login.html"
+}
 
+function track(id){
+setStorage("trackid",id)
+window.location.href="track.html"
+}
 const getStorage = (item) => (JSON.parse(localStorage.getItem(item)))
 
 const setStorage = (item, data) => (localStorage.setItem(item, JSON.stringify(data)))
 
 const removeStorage = (item) => (localStorage.removeItem(item))
+
