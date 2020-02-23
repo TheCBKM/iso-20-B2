1ac8ea635573c6eeb2bd698aa99909a063b850ae
prakhar-mangal <mangalprakhar07@gmail.com>
2020-02-23 19:20:29 +0530

    integration

--- util.js
+++ util.js
@@ -1,4 +1,5 @@
-const url = 'https://isogst.herokuapp.com/'
+// const url = 'https://isogst.herokuapp.com/'
+const url = 'http://127.0.0.1:3000/'
 
 
 const getStorage = (item) => (JSON.parse(localStorage.getItem(item)))
